116b6d538fb358d536b0e55958a11b0f766a0fe6
xianyu <xianyu@iiisland.com>
2020-11-23 10:46:34 +0800

    v0.1.0 finshed

--- utils/index.js
+++ utils/index.js
@@ -9,22 +9,21 @@ function hasDir(path) {
     return fs.existsSync(path)
 }
 
-function delDir (path) {
-    var files = [];
-    if(fs.existsSync(path)) {
-        files = fs.readdirSync(path);
-        files.forEach(function(file, index) {
-            var curPath = path + "/" + file;
-            if(fs.statSync(curPath).isDirectory()) {
-                deleteall(curPath);
+function delDir(path) {
+    var files = []
+    if (fs.existsSync(path)) {
+        files = fs.readdirSync(path)
+        files.forEach(function (file, index) {
+            var curPath = path + '/' + file
+            if (fs.statSync(curPath).isDirectory()) {
+                deleteall(curPath)
             } else {
-                fs.unlinkSync(curPath);
+                fs.unlinkSync(curPath)
             }
-        });
-        fs.rmdirSync(path);
+        })
+        fs.rmdirSync(path)
     }
-};
-
+}
 
 function getFullYear() {
     return new Date().getFullYear()
